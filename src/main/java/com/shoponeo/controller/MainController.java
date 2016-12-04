1026f0f4d245b52061f7b0c29b9ca6bc06282700
TomOw <owczarczyk.tomasz@wp.pl>
2016-12-05 00:50:10 +0100

    Logger example

--- src/main/java/com/shoponeo/controller/MainController.java
+++ src/main/java/com/shoponeo/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.shoponeo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+    private final Logger logger = LoggerFactory.getLogger(MainController.class);
+
     @RequestMapping(value = "/")
     public String mainView() {
+        logger.info("getting main view");
         return "main";
     }
 }
