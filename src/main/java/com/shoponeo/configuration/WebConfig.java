6e8f87af36503960d0bef56b17e0788871a2a47b
TomOw <owczarczyk.tomasz@wp.pl>
2016-12-20 21:29:40 +0100

    commented out the CORSConfig in WebConfig class

--- src/main/java/com/shoponeo/configuration/WebConfig.java
+++ src/main/java/com/shoponeo/configuration/WebConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.shoponeo.configuration;
 
 import org.springframework.context.annotation.Configuration;
@@ -5,20 +6,23 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+*/
 /**
  * Created by Tomasz on 20.12.2016.
- */
+ *//*
+
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("*/
+/**")
                 .allowedOrigins("http://domain2.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("header1", "header2", "header3")
                 .exposedHeaders("header1", "header2")
                 .allowCredentials(false).maxAge(3600);
     }
-}
+}*/
