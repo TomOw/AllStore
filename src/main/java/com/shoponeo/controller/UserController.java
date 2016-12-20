cdb8713376bf2f320f88780e71148297d58d6ce3
TomOw <owczarczyk.tomasz@wp.pl>
2016-12-20 22:04:14 +0100

    changed getLoggedUser method to return com.shoponeo.model.user

--- src/main/java/com/shoponeo/controller/UserController.java
+++ src/main/java/com/shoponeo/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.shoponeo.controller;
 
+import com.shoponeo.model.User;
+import com.shoponeo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +17,11 @@ import java.security.Principal;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @RequestMapping("/me")
-    public Principal getLoggedInUser(Principal principal) {
-        return principal;
+    public User getLoggedInUser(Principal principal) {
+        return userRepository.get(principal.getName());
     }
 }
