c748a4bb68774b8d90872af2ce1853f2d657fdec
TomOw <owczarczyk.tomasz@wp.pl>
2016-12-28 00:56:42 +0100

    added method for getting list of orders by store

--- src/main/java/com/shoponeo/controller/StoreController.java
+++ src/main/java/com/shoponeo/controller/StoreController.java
@@ -1,12 +1,16 @@
 package com.shoponeo.controller;
 
 import com.shoponeo.model.shop.Address;
+import com.shoponeo.model.shop.Order;
 import com.shoponeo.model.shop.Store;
+import com.shoponeo.repository.OrderRepository;
 import com.shoponeo.repository.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by Tomasz on 20.11.2016.
@@ -18,6 +22,9 @@ public class StoreController {
     @Autowired
     StoreRepository storeRepository;
 
+    @Autowired
+    OrderRepository orderRepository;
+
     @RequestMapping(value = "/address/{storeName}")
     public Address getStoreAddresByStoreName(@PathVariable("storeName") String name) {
         return storeRepository.getStoreAddressByStoreName(name);
@@ -26,5 +33,10 @@ public class StoreController {
     @RequestMapping(value = "/byName/{storeName}")
     public Store getStoreByName(@PathVariable("storeName") String name) {
         return storeRepository.getStoreByName(name).get(0);
+    }
+
+    @RequestMapping(value = "/orders/{storeName}")
+    public List<Order> getOrdersFromStore(@PathVariable("storeName") String name) {
+        return orderRepository.getOrdersByStoreName(name);
     }
 }
