f685e947c25fe199a853f280e0ca50c942d52bf1
TomOw <owczarczyk.tomasz@wp.pl>
2017-01-23 22:27:35 +0100

    added equals and hashCode to User class

--- src/main/java/com/shoponeo/model/User.java
+++ src/main/java/com/shoponeo/model/User.java
@@ -107,6 +107,25 @@ public class User implements UserDetails {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (getUsername() != null ? !getUsername().equals(user.getUsername()) : user.getUsername() != null)
+            return false;
+        return getPassword() != null ? getPassword().equals(user.getPassword()) : user.getPassword() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getUsername() != null ? getUsername().hashCode() : 0;
+        result = 31 * result + (getPassword() != null ? getPassword().hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return this.authorities;
     }
